c354aee0da789f49d13b477b9939a457b1b850b7
0xTomas <99558481+0xTomas@users.noreply.github.com>
2022-04-18 19:15:48 +1000

    chore: update reduce() function

--- JavaScript/credit-card-checker-starter/reduce.js
+++ JavaScript/credit-card-checker-starter/reduce.js
@@ -2,7 +2,6 @@
 const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
 
 const sum = valid1.reduce((total, currentNumber, currentIndex) => {
-    let index = 0
     // console.log(total)
     if (currentIndex % 2 === 0) {
         return total + currentNumber
